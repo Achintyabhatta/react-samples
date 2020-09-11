b3d1ab916ecfdf251051e4c1d166e05aab63350b
System Administrator <achintya96bhatta@gmail.com>
2020-09-11 07:48:15 +0530

    second commit

--- ImageSearch/src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers:{
-        Authorization: 'Client-ID F_e8thl9w4GO2JbSidISlitc9qlgRcC4OxXEQgwsmS0'
-    }
-});
